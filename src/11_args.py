afd869c53c8f2bcfbd08fa50364824f6ef32f4d7
Darragh Ferry <darraghferry@hotmail.com>
2020-04-07 17:43:12 +0100

    classes

--- src/11_args.py
+++ src/11_args.py
@@ -21,10 +21,14 @@ print(f1(1, 2))
 
 
 def f2(*args):
-    sum = 0
+    total = 0
     for arg in args:
-        sum = sum + arg
-    return sum
+        if isinstance(arg, (list, tuple)):
+            for i in arg:
+                total = total + i
+        else:
+            total = total + arg
+    return total
 
 
 print(f2(1))                    # Should print 1
@@ -35,7 +39,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(*a))    # Should print 22
+print(f2(a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
